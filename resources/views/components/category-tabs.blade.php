470a2fd8e77cb8cba9448a8fe054635519231ce6
Tim Jossund <timjossund@gmail.com>
2025-05-02 18:13:52 -0500

    adding more tailwind

---
+++ resources/views/components/category-tabs.blade.php
@@ -0,0 +1,8 @@
+<a href="/dashboard" class="{{ Request::segment(3) == "" ? "rounded-md bg-blue-100 px-3 py-2 text-sm font-medium text-blue-700 active" : "rounded-md px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700" }} rounded-lg flex justify-center items-center" aria-current="page">
+    All
+</a>
+@foreach ($categories as $category)
+<a href="/dashboard/{{ $category->title }}" class="inline-block px-4 py-3 {{ Request::segment(3) == $category->title ? "rounded-md bg-indigo-100 px-3 py-2 text-sm font-medium text-indigo-700 active" : "rounded-md px-3 py-2 text-sm font-medium text-gray-500 hover:text-gray-700" }} rounded-lg" aria-current="page">
+    {{ $category->title }}
+</a>
+@endforeach
