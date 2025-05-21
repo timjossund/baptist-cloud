e9d2da1d9b50aa75ac4b2befeaab9fe017412e84
Tim Jossund <timjossund@gmail.com>
2025-05-21 13:35:35 -0500

    removeing irrelavant words

--- resources/views/create-post.blade.php
+++ resources/views/create-post.blade.php
@@ -12,8 +12,6 @@
                         <input
                             class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
                             aria-describedby="file_input_help" id="image" type="file" name="image">
-                        <p class="mt-1 text-sm text-gray-500" id="file_input_help">Supported format: JPG (MAX.
-                            800x400px).</p>
                         <x-input-error :messages="$errors->get('image')" class="mt-2" />
                     </div>
                     {{-- Post Title --}}
@@ -23,7 +21,7 @@
                             name="title" :value="old('title')" autofocus />
                         <x-input-error :messages="$errors->get('title')" class="mt-2" />
                     </div>
-                    {{--                    Post Category --}}
+                    {{-- Post Category --}}
                     <div>
                         <x-input-label for="category_id" :value="__('Category:')" />
                         <select name="category_id" id="category_id" class="block border mt-1 w-full text-xl p-2">
@@ -35,14 +33,14 @@
                         </select>
                         <x-input-error :messages="$errors->get('category_id')" class="mt-2" />
                     </div>
-                    {{--                    Post Body --}}
+                    {{-- Post Body --}}
                     <div class="mt-2">
                         <label for="content" class="text-lg text-gray-700">Body Content</label>
                         <textarea id="content" class="hidden" name="content">{!! old('content') !!}</textarea>
                         <div id="bodycontent">{!! old('content') !!}</div>
                         <x-input-error :messages="$errors->get('content')" class="mt-2" />
                     </div>
-                    {{--                    Post Submit --}}
+                    {{-- Post Submit --}}
                     <x-primary-button class="text-white max-w-52 flex justify-center text-center py-2 rounded-lg"
                         type="submit">Save Draft</x-primary-button>
                 </form>
