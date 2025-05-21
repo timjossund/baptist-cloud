9e16b042f095f3e39e50967e65919a51d448c150
Tim Jossund <timjossund@gmail.com>
2025-05-20 21:28:16 -0500

    last update push

--- resources/views/create-listing.blade.php
+++ resources/views/create-listing.blade.php
@@ -3,8 +3,7 @@
         <div class="max-w-7xl mx-auto px-5 w-full">
             <div class="bg-white flex flex-wrap items-center sm:py-12 mx-auto px-6 lg:px-8 rounded-lg shadow-sm sm:rounded-lg">
                 <h2 class="text-4xl font-bold mb-8 max-w-6xl w-full">Create A New Listing</h2>
-                <form action="/post/create-post" method="POST" enctype="multipart/form-data"
-                      class="w-full max-w-3xl flex flex-wrap justify-between">
+                <form action="/post/create-post" method="POST" enctype="multipart/form-data" class="w-full max-w-3xl flex flex-wrap justify-between">
                     @csrf
                     {{--                    Post Image --}}
                     <div class="w-full">
