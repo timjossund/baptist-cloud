4666e6dc53e2cfb7e617cafc8a1678bf1ff96906
Tim Jossund <timjossund@gmail.com>
2025-05-30 13:20:55 -0500

    adding some new alpine js animations

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,14 +55,24 @@
             </div>
         </div>
         @if (session()->has('success'))
-            <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show" class="fixed py-4 px-12 bottom-10 right-10 max-w-80 shadow-lg z-10 bg-white rounded-lg">
+            <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition:enter="transition ease-out duration-300"
+                 x-transition:enter-start="opacity-0 scale-90"
+                 x-transition:enter-end="opacity-100 scale-100"
+                 x-transition:leave="transition ease-in duration-300"
+                 x-transition:leave-start="opacity-100 scale-100"
+                 x-transition:leave-end="opacity-0 scale-90" class="fixed py-4 px-12 bottom-10 right-10 max-w-80 shadow-lg z-10 bg-white rounded-lg">
                 <div class="w-full text-center text-lg text-blue-700">
                     {{ session('success') }}
                 </div>
             </div>
         @endif
         @if (session()->has('error'))
-            <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show" class="fixed py-4 px-12 bottom-10 right-10 max-w-80 bg-red-300 z-10 rounded-lg shadow-lg">
+            <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition:enter="transition ease-out duration-300"
+                 x-transition:enter-start="opacity-0 scale-90"
+                 x-transition:enter-end="opacity-100 scale-100"
+                 x-transition:leave="transition ease-in duration-300"
+                 x-transition:leave-start="opacity-100 scale-100"
+                 x-transition:leave-end="opacity-0 scale-90" class="fixed py-4 px-12 bottom-10 right-10 max-w-80 bg-red-300 z-10 rounded-lg shadow-lg">
                 <div class="w-full text-center text-lg">
                     {{ session('error') }}
                 </div>
