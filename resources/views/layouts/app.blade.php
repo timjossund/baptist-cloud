ad34f0258b1be92611f8339cb5378e7c0d5d53d5
Tim Jossund <timjossund@gmail.com>
2025-05-30 13:29:44 -0500

    updating some styles for flash notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,24 +55,32 @@
             </div>
         </div>
         @if (session()->has('success'))
-            <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition:enter="transition ease-out duration-300"
+            <div x-data="{show: true}"
+                 x-init="setTimeout(() => show = false, 3000)"
+                 x-show="show"
+                 x-transition:enter="transition ease-out duration-300"
                  x-transition:enter-start="opacity-0 scale-90"
                  x-transition:enter-end="opacity-100 scale-100"
                  x-transition:leave="transition ease-in duration-300"
                  x-transition:leave-start="opacity-100 scale-100"
-                 x-transition:leave-end="opacity-0 scale-90" class="fixed py-4 px-12 bottom-10 right-10 max-w-80 shadow-lg z-10 bg-white rounded-lg">
-                <div class="w-full text-center text-lg text-blue-700">
+                 x-transition:leave-end="opacity-0 scale-90"
+                 class="fixed py-4 px-12 bottom-10 right-10 max-w-80 shadow-lg z-10 bg-blue-600 rounded-lg">
+                <div class="w-full text-center text-lg text-white">
                     {{ session('success') }}
                 </div>
             </div>
         @endif
         @if (session()->has('error'))
-            <div x-data="{show: true}" x-init="setTimeout(() => show = false, 3000)" x-show="show" x-transition:enter="transition ease-out duration-300"
+            <div x-data="{show: true}"
+                 x-init="setTimeout(() => show = false, 3000)"
+                 x-show="show"
+                 x-transition:enter="transition ease-out duration-300"
                  x-transition:enter-start="opacity-0 scale-90"
                  x-transition:enter-end="opacity-100 scale-100"
                  x-transition:leave="transition ease-in duration-300"
                  x-transition:leave-start="opacity-100 scale-100"
-                 x-transition:leave-end="opacity-0 scale-90" class="fixed py-4 px-12 bottom-10 right-10 max-w-80 bg-red-300 z-10 rounded-lg shadow-lg">
+                 x-transition:leave-end="opacity-0 scale-90"
+                 class="fixed py-4 px-12 bottom-10 right-10 max-w-80 bg-red-300 z-10 rounded-lg shadow-lg">
                 <div class="w-full text-center text-lg">
                     {{ session('error') }}
                 </div>
