c1e7dc173f6cf1e4dbe32b0c2f9329c9b946b478
Tim Jossund <timjossund@gmail.com>
2025-05-31 18:52:44 -0500

    fix for save draft btn

--- resources/views/edit-post.blade.php
+++ resources/views/edit-post.blade.php
@@ -53,9 +53,11 @@
                     </div>
                     {{-- Post Submit --}}
                     <div class="flex gap-4">
-                        <x-primary-button class="text-white max-w-32 flex justify-center text-center py-2 rounded-lg" type="submit">
-                            Save
+                        @if (!$post->published_at)
+                        <x-primary-button class="text-white max-w-44 flex justify-center text-center py-2 rounded-lg" type="submit">
+                            Save Draft
                         </x-primary-button>
+                        @endif
                         <div class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-lg text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150" @click="publish = true">
                             Publish
                         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
 
     <!-- Scripts -->
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />
+{{--    <link href="https://cdn.quilljs.com/1.3.6/quill.snow.css" rel="stylesheet" />--}}
     @livewireStyles
 </head>
 
