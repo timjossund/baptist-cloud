3eca5a82a3dae6a8ea39a2d4bbd245a6721e1110
Tim Jossund <timjossund@gmail.com>
2025-06-03 16:46:54 -0500

    tweaking styles

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,33 +1,34 @@
 <div class="flex flex-wrap">
     <input type="text" wire:model.live="search" class="w-full" placeholder="Start typing a search..." autofocus>
-    <div class="w-1/2 mt-4 pr-4">
+    <div class="w-full md:w-1/2 mt-4 md:pr-4">
+        @if ($search)
         <h3 class="mb-2">Results in users:</h3>
         <ul class="flex flex-col gap-4">
-        @if ($search)
+
         @foreach ($users as $user)
             <a href="{{ route('public-profile', $user->username) }}" wire:navigate>
                 <li wire:key="{{ $user->id }}" class="w-full shadow-md py-2 px-4 flex gap-4 items-center rounded">
                     <img src="{{ $user->avatar }}" alt="" class="aspect-[1/1] h-10 w-10 rounded-full">
-                    <h4>{{ $user->name }}</h4>
+                    <h4 class="text-sm md:text-md">{{ $user->name }}</h4>
                 </li>
             </a>
         @endforeach
-        @endif
         </ul>
+        @endif
     </div>
-    <div class="w-1/2 mt-4">
+    <div class="w-full md:w-1/2 mt-4">
+        @if ($search)
         <h3 class="mb-2">Results in articles:</h3>
         <ul class="flex flex-col gap-4">
-        @if ($search)
         @foreach ($posts as $post)
             <a href="{{ route('single-post', ['username' => $post->user->username, 'post' => $post->slug]) }}">
                 <li wire:key="{{ $post->id }}" class="w-full shadow-md py-2 px-4 flex gap-4 items-center rounded">
                     <img src="{{ $post->image }}" alt="" class="aspect-[1/1] h-10 w-10 rounded">
-                    <h4>{{ $post->title }}</h4>
+                    <h4 class="text-sm md:text-md">{{ $post->title }}</h4>
                 </li>
             </a>
         @endforeach
-        @endif
         </ul>
+        @endif
     </div>
 </div>
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto mt-10 px-5 w-full">
-        <div class="bg-white sm:py-12 mx-auto px-6 lg:px-8 rounded-lg shadow-sm sm:rounded-lg flex flex-col">
+        <div class="bg-white p-10 mx-auto px-6 lg:px-8 rounded-lg shadow-sm sm:rounded-lg flex flex-col">
             <h2 class="text-4xl font-bold max-w-6xl w-full">Search Users & Articles</h2>
             <p class="mb-4 max-w-6xl w-full">This will return users and article titles matching your entry.</p>
             <livewire:search />
