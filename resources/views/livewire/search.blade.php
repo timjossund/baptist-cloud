367b580092b7b9366584c796fdec13d95894fbfe
Tim Jossund <timjossund@gmail.com>
2025-06-03 16:08:42 -0500

    tweaking search

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,7 +1,7 @@
 <div class="flex flex-wrap">
     <input type="text" wire:model.live="search" class="w-full" placeholder="Start typing a search..." autofocus>
-    <div class="w-1/2 mt-4 px-4">
-        <h3 class="mb-2">Results in Authors:</h3>
+    <div class="w-1/2 mt-4 pr-4">
+        <h3 class="mb-2">Results in users:</h3>
         <ul class="flex flex-col gap-4">
         @if ($search)
         @foreach ($users as $user)
@@ -16,7 +16,7 @@
         </ul>
     </div>
     <div class="w-1/2 mt-4">
-        <h3 class="mb-2">Results in Articles:</h3>
+        <h3 class="mb-2">Results in articles:</h3>
         <ul class="flex flex-col gap-4">
         @if ($search)
         @foreach ($posts as $post)
--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -1,7 +1,8 @@
 <x-app-layout>
     <div class="max-w-7xl mx-auto mt-10 px-5 w-full">
         <div class="bg-white sm:py-12 mx-auto px-6 lg:px-8 rounded-lg shadow-sm sm:rounded-lg flex flex-col">
-            <h2 class="text-4xl font-bold mb-8 max-w-6xl w-full">Search here for Authors & Articles</h2>
+            <h2 class="text-4xl font-bold max-w-6xl w-full">Search Users & Articles</h2>
+            <p class="mb-4 max-w-6xl w-full">This will return users and article titles matching your entry.</p>
             <livewire:search />
         </div>
     </div>
