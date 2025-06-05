063b3ee3c0eb080cd24c8a386f64df830d1f91b4
Tim Jossund <timjossund@gmail.com>
2025-06-05 09:14:44 -0500

    fixing some slow network issues

--- resources/views/livewire/post-form.blade.php
+++ resources/views/livewire/post-form.blade.php
@@ -1,11 +1,11 @@
 <form wire:submit="store" class="m-auto w-full max-w-6xl flex flex-col gap-4">
     @csrf
     {{-- Post Image --}}
-    <div>
+    <div class="relative">
         @if ($image)
             <img src="{{ $image->temporaryUrl() }}" alt="" class="w-full rounded-xl object-cover mb-12 aspect-[16/9]">
         @endif
-        <x-input-label class="block mb-2" for="image">Featured Image: this will be cropped to a 16:9 ratio</x-input-label>
+        <x-input-label class="block mb-2" for="image">Featured Image:<span class="text-sm text-gray-500"> This will be cropped to a 16:9 ratio. Please note: If your network is slow, you may not see the image until it has finished loading.</span></x-input-label>
         <input class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:border-indigo-500" aria-describedby="file_input_help" id="image" type="file" name="image" wire:model="image" autofocus>
         <x-input-error :messages="$errors->get('image')" class="mt-2" />
     </div>
