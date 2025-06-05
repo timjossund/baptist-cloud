46ca2dd66002b0cd44d1086f916c43f4938939ea
Tim Jossund <timjossund@gmail.com>
2025-06-05 11:19:05 -0500

    adding margin to form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -45,7 +45,7 @@
             </div>
 
             <!-- Confirm Password -->
-            <div class="mt-4">
+            <div class="mt-4 mb-4">
                 <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
                 <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password" name="password_confirmation" autocomplete="new-password" />
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
