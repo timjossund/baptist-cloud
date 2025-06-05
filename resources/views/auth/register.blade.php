92ad44bafe83523bd6a5d97361b4d104bef551fd
Tim Jossund <timjossund@gmail.com>
2025-06-05 14:14:40 -0500

    removing some bloat

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -55,8 +55,6 @@
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                     {{ __('Already registered?') }}
                 </a>
-                
-
                 <x-primary-button class="ms-4">
                     {{ __('Register') }}
                 </x-primary-button>
