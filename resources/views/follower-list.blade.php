462fbc6ec7577d2375925e8e7583b7eba98f26ee
Tim Jossund <timjossund@gmail.com>
2025-08-23 14:09:53 -0500

    adding some new elements to the followers view and new styling

--- resources/views/follower-list.blade.php
+++ resources/views/follower-list.blade.php
@@ -5,15 +5,24 @@
                 <h1 class="text-4xl">Your Followers: {{ auth()->user()->followers()->count() }}</h1>
                 <a href="{{ route('public-profile', auth()->user()) }}"><x-primary-button class="flex justify-center">Back To Profile</x-primary-button></a>
             </div>
-            <div class="flex flex-col border-t-4 border-gray-200 py-4">
+            <div class="flex flex-col border-t-4 border-gray-200">
+                <div class="flex items-center w-full justify-between border-b-4 bg-gray-100">
+                    <p class="ml-2 text-md font-bold w-1/3">Name:</p>
+                    <p class="ml-2 text-md font-bold w-1/3">Username:</p>
+                    <p class="ml-2 text-md font-bold w-1/3">Position:</p>
+                </div>
                 @forelse( auth()->user()->followers as $follower )
                     <a href="{{ route('public-profile', $follower) }}" wire:navigate class="flex items-center border-b-2 border-gray-200 p-2 first:border-t-2 min-w-56 justify-between">
-                        <div class="flex items-center">
+                        <div class="flex items-center w-1/3">
                             <img src="{{ $follower->avatar }}" alt="{{ $follower->username }}" class="rounded-full h-10 w-10">
                             <p class="ml-2 text-md">{{ $follower->name }}</p>
                         </div>
-                        <p class="ml-2 text-md mr-2">Username: {{ $follower->username }}</p>
-                        <p class="ml-2 text-md mr-2">Position: {{ $follower->bio }}</p>
+                        <div class="flex items-center w-1/3">
+                            <p class="ml-2 text-md mr-2">{{ $follower->username }}</p>
+                        </div>
+                        <div class="flex items-center w-1/3">
+                            <p class="ml-2 text-md mr-2">{{ $follower->bio }}</p>
+                        </div>
                     </a>
                 @empty
                     <h6 class="text-sm text-center">- No Followers To List -</h6>
