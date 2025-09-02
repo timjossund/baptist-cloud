cbc0c4bc9ee2f4066ec10ef1618cd82393f01555
Tim Jossund <timjossund@gmail.com>
2025-09-02 10:09:10 -0500

    fixing a typo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -46,7 +46,7 @@
         <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
             <p class="text-center text-gray-500">
                 © {{ date('Y') }} {{ config('app.name') }}.
-                All rights reserved.
+                All Rights Reserved.
             </p>
         </div>
     </div>
