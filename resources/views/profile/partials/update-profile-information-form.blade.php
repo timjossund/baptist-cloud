0672a112f12761fd5ebf95b2112642df784944c4
Tim Jossund <timjossund@gmail.com>
2026-07-20 10:12:31 -0500

    adding

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -13,8 +13,9 @@
         @csrf
     </form>
 
-    <form method="post" action="{{ route('profile.update', absolute: false) }}" class="mt-6 space-y-6" enctype="multipart/form-data">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 space-y-6" enctype="multipart/form-data">
         @csrf
+        @method('patch')
 
         <div>
             @if (auth()->user()->avatar)
@@ -24,8 +25,7 @@
             <x-input-label class="block mb-2" for="avatar">Current Avatar</x-input-label>
             <input
                 class="block w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 focus:outline-none"
-                aria-describedby="file_input_help" id="avatar" type="file" name="avatar" accept="image/jpeg,image/png,image/jpg">
-            <p id="file_input_help" class="mt-1 text-xs text-gray-500">JPG or PNG, max 2&nbsp;MB.</p>
+                aria-describedby="file_input_help" id="avatar" type="file" name="avatar">
             <x-input-error :messages="$errors->get('avatar')" class="mt-2" />
         </div>
 
